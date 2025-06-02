be535e9f042cea2478c595fc6f6182a2c208177b
Asif Abbas <asif@inestweb.com>
2025-06-02 12:32:45 +0530

    Added OramaAPI.sln file

---
+++ Orama API/Data/UserDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Orama_API.Data;
+
+namespace Orama_API.Data
+{
+    public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+            optionsBuilder.UseSqlite("Data Source=alpha.db"); // ✅ Use your actual DB connection string
+
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
+}
